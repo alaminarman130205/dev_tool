e40e2ad3558c9fc09b28f35b8444d2b83d671582
alaminarmanjanina <81838250+alaminarmanjanina@users.noreply.github.com>
2022-09-10 22:57:44 +0600

    first  commit

---
+++ error.js
@@ -0,0 +1,17 @@
+// syntex error 
+// cont f = 10; ai type ar error hocce syntex error 
+
+
+// type error 
+// let address ; 
+// console.log(address.length);
+// ai quality error hocce type error 
+
+
+// refference error 
+// scope ar bahire thakle refference error hobe 
+// function   add(a,b){
+//     const result = a+b; 
+//     return result;   
+// }
+// console.log(result);
